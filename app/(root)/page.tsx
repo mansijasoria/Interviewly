382efe15b502f66ec2f5c5eb2a70d0cabc2fe420
mansijasoria <mansijasoria13@gmail.com>
2025-04-13 18:25:37 +0530

    Basic Navbar done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Home page</div>
+   <>
+   <section className='card-cta'>
+    <div className='flex flex-col gap-6 max-w-lg'>
+      <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+      <p className='text-lg'>
+        Practice on real interview questions & get instant feedback
+      </p>
+      <Button asChild className='btn-primary max-sm:w-full'>
+        <Link href='/interview'>Start an Intervirew</Link>
+      </Button>
+    </div>
+   </section>
+   </>
   )
 }
 
