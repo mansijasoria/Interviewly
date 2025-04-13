888e6521272e214aa75b4361a9182c2537df1e71
mansijasoria <mansijasoria13@gmail.com>
2025-04-13 19:41:25 +0530

    Basic home section done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,14 +4,16 @@ import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div className='root-layout'>
+    <div className='root-layout'> 
       <nav>
-        <Link href='/' className='flex items-center gap-2'>
+        <Link href="/" className='flex items-center gap-2'>
         <Image src="/logo.svg" alt='logo' width={38} height={32} />
         <h2 className='text-primary-100'>Interviewly</h2>
         </Link>
       </nav>
+      {children}
     </div>
+    
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -14,6 +15,23 @@ const page = () => {
       <Button asChild className='btn-primary max-sm:w-full'>
         <Link href='/interview'>Start an Intervirew</Link>
       </Button>
+    </div>
+
+    <Image src="/robot.png" alt=""  width={400} height={400} className='max-sm:hidden'/>
+   </section>
+
+   <section className='flex flex-col gap-6 mt-8'>
+    <h2>Your Interviews</h2>
+    <div className='interviews-section'>
+      <p>You haven&apos;t taken any interviews yet</p>
+    </div>
+   </section>
+
+   <section className='flex flex-col gap-6 mt-8'>
+    <h2>Take an Interview</h2>
+
+    <div className='interviews-section'>
+      <p>There are no interviews available</p>
     </div>
    </section>
    </>
